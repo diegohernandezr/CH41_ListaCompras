3b1ac60738bfda77372678c37d3ac2fd93b1ad89
Diego <diegohrnz12@gmail.com>
2024-05-29 08:58:48 -0600

    se actualizó

--- js/main.js
+++ js/main.js
@@ -1,42 +1,48 @@
 /// El código va aquí ///
 //codigo va aqui//
-
 let btnAgregar = document.getElementById("btnAgregar");
 let btnClear = document.getElementById("btnClear");
 
-let txtNombre = document.getElementById("txtNombre");
-let txtNumber = document.getElementById("txtNumber");
+let txtNombre = document.getElementById("Name");
+let txtNumber = document.getElementById("Number");
 
 let alertValidaciones = document.getElementById("alertValidaciones");
-let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+let alertValidacionesTexto  = document.getElementById("alertValidacionesTexto");
 
-function validarCantidad() {
-  if (txtNombre.value.lenght == 0) {
-    return false;
-  }// if lenght
-  return true;
-}//validar cantidad
+function validarCantidad(){
+    if(txtNumber.value.length==0){
+        return false;
+    }//if length
+    return true;
+}//validarCantidadname
 
-btnAgregar.addEventListener("click", function (event) {
-  event.preventDefault();
-    alertValidacionesTexto.innerHTML = "";
-    alertValidaciones.style.display = "none";
-    txtNombre.style.border = "";
-    txtNumber.style.border = "";
-  if (txtNombre.value.lenght <3) {
-    alertValidacionesTexto.innerHTML = "El <strong> nombre <strong/> no es correcto";
-    alertValidacionesTexto.style.display = "block";
-    txtNombre.style.border = "solid red medium";
-  }
-  if (!validarCantidad()) {
-    alertValidacionesTexto.innerHTML += " El <strong>Numero</strong> no es correcto";
-    alertValidaciones.style.display = "block";
-    txtNombre.style.border = "solid red medium";
-  }
+btnAgregar.addEventListener("click", function(event){
+    event.preventDefault();
+    alertValidacionesTexto.innerHTML="";
+    alertValidaciones.style.display="none";
+    txtNombre.style.border="";
+    txtNumber.style.border="";
+    if (txtNombre.value.length<3){
+        alertValidacionesTexto.innerHTML="El <strong>Nombre</strong> no es correcto<br/>";
+        alertValidaciones.style.display="block";
+        txtNombre.style.border="solid red medium";
+    }//length<3
+   if(! validarCantidad()){
+        alertValidacionesTexto.innerHTML+="El <strong>Número</strong> no es correcto";
+        alertValidaciones.style.display="block";
+        txtNumber.style.border="solid red medium";
+   }//!validarCantidad
+
+
 });
 
-btnClear.addEventListener("click", function (event) {
-  event.preventDefault();
-  txtNombre.value = "";
-  txtNumber.value = "";
+btnClear.addEventListener("click", function(event){
+    event.preventDefault();
+
+    txtNombre.value = "";
+    txtNumber.value = "";
+    alertValidacionesTexto.innerHTML="";
+    alertValidaciones.style.display="none";
+    txtNombre.style.border="";
+    txtNumber.style.border="";
 });
